cd7f04ecd302a11fe16de340fb822afbcc455c11
Merve Alkurt <mrvalkrtt97@gmail.com>
2024-08-24 14:56:20 -0500

    new4 version

---
+++ src/C40_ToStringCompositionMain.java
@@ -0,0 +1,9 @@
+import javax.naming.ldap.Control;
+
+public class C40_ToStringMain {
+
+    public static void main(String[] args) {
+
+        C39_ToString toStringObj = new C39_ToString(4,5,6);
+    }
+}
